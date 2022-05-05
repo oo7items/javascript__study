9deceb92bfbadd594876e895b2ed46f24ad3bb61
张国正 <oo7items@gmail.com>
2022-05-05 19:13:34 +0800

    1

--- complete-javascript-course-master/06-Modal/final/script.js
+++ complete-javascript-course-master/06-Modal/final/script.js
@@ -22,7 +22,7 @@ btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
-  // console.log(e.key);
+  console.log(e);
 
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
