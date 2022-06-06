b7161ffee44868e49d520ed57d0036cca54661c4
oo7items <oo7items@gmail.com>
2022-06-06 18:05:01 +0800

    1

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -85,17 +85,19 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // });
 
 const displayMovements = function (movement) {
-  containerMovements.textContent = '';
+  containerMovements.innerHTML = '&amp;';
+  //
+  // .textContent = 0
   movement.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
-      <div class="movements__row">
-        <div class="movements__type movements__type--${type}">${
+                <div class="movements__row">
+                  <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${Math.abs(mov)}€</div>
-      </div>
-    `;
+                  <div class="movements__value">${Math.abs(mov)}€</div>
+                </div>
+              `;
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
